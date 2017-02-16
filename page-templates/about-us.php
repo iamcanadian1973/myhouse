9eeebbc87d5a08522d1a2bc545f6390dac1c7837
Kyle Rumble <kylerumble@gmail.com>
2017-02-16 15:16:59 -0800

    About us fixes.

--- page-templates/about-us.php
+++ page-templates/about-us.php
@@ -10,7 +10,7 @@ get_header(); ?>
 get_template_part( 'template-parts/section', 'hero' );
 ?>
 
-z<div id="primary" class="content-area">
+<div id="primary" class="content-area">
 
 	<main id="main" class="site-main" role="main">
 	<?php
